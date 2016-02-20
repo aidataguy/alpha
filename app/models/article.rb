b00d4e8b5d94eed4e8107468469ffb87a2ba8693
thedesignable <himanshu-patel@live.com>
2016-02-21 00:47:16 +0530

    Added article form and functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title, :excerpt,  presence: true, length: ( minimum: 10, maximum: 50)
-	validates :description, presence: true, length: ( minimum: 25 )
+	validates :title, :excerpt,  presence: true, length: { minimum: 10, maximum: 50}
+	validates :excerpt,  presence: true, length: { minimum: 5, maximum: 50}
+	validates :description, presence: true, length: { minimum: 10}
 end
