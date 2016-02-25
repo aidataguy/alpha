a043a6bf8b9d44309534f241a9d05adb5b265c5f
thedesignable <himanshu-patel@live.com>
2016-02-25 11:29:57 +0530

    Added Admin User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 	before_action :set_user, only:[:edit, :update, :show]
-	before_action :require_same_user, only:[:edit, :update]
+	before_action :require_same_user, only:[:edit, :update, :destroy]
+	before_action :require_admin, only:[:destroy]
+
 
 	def index
 		@users = User.paginate(page: params[:page], per_page: 3)
@@ -13,7 +15,9 @@ class UsersController < ApplicationController
 
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
+			# this will do sign in at the time of signup
+			session[:user_id] = @user.id 
+			# rest is flash message and stuff 
 			flash[:success] = "User Created, welcome to the blog #{@user.username}"
 			redirect_to articles_path
 		else
@@ -28,6 +32,13 @@ class UsersController < ApplicationController
 			@user_articles = @user.articles.paginate(page: params[:page], per_page:5)
 		end
 
+		def destroy
+			@user = User.find(params[:id])
+			@user.destroy
+			flash[:danger] = "User and Associated aritcles have been deleted"
+			redirect_to users_path
+		end
+
 		def update
 			if @user.update(user_params)
 				flash[:success] = "your accout info has been updated"
@@ -36,6 +47,7 @@ class UsersController < ApplicationController
 				render 'edit'
 			end
 		end
+
 		
 
 	private
@@ -43,12 +55,19 @@ class UsersController < ApplicationController
 		params.require(:user).permit(:username, :email, :password)
 	end
 	def require_same_user
-		if current_user != @user
+		if current_user != @user && !current_user.admin?
 			flash[:danger] = "you can only edit your own account"
 		redirect_to root_path
 		end
 	end
 	def set_user
 		@user = User.find(params[:id])
+	end
+
+	def require_admin
+		if logged_in? && !current_user.admin?
+			flash[:danger] = "Only Admin is supposed to do that "
+		redirect_to root_path
+		end
 	end
 end
